f1ded1c9119472da4042d221e9525b9187cdda67
raria029 <rocio.arias98@gmail.com>
2021-07-12 19:43:19 -0400

    js adjustments (var additions)

--- script.js
+++ script.js
@@ -1,54 +1,30 @@
-// Number & Range Selector/ Syncing the two
-const numberselector = document.getElementById ('numberselector')
-const rangeselector = document.getElementById ('rangeselector')
-numberselector.addEventListener('input', syncselector)
-rangeselector.addEventListener('input', syncselector)
-function syncselector(e) {
-  const value = e.target.value
-  numberselector.value = value
-  rangeselector.value = value
-}
-//Lowercase, Uppercase, Numbers, Special Characters
-const lowercaseletters = document.getElementById('lowercaseletters')
-const uppercaseletters = document.getElementById('uppercaseletters')
-const numbers = document.getElementById('numbers')
-const specialcharacters = document.getElementById('specialcharacters')
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-//Arrays to generate
-const lowercase_char_code = arrayfromlowtohigh(97,122)
-const uppercase_char_code = arrayfromlowtohigh(65,90)
-const numbers_char_code = arrayfromlowtohigh(48,57)
-const specialcharacters_char_code = arrayfromlowtohigh(33,47).concat(
+// Char codes as variables
+var lowercaseletter = arrayfromlowtohigh(97,122)
+var uppercaseletters = arrayfromlowtohigh(65,90)
+var numbers = arrayfromlowtohigh(48,57)
+var specialcharacters = arrayfromlowtohigh(33,47).concat(
   arrayfromlowtohigh(58,64)
 ).concat(
   arrayfromlowtohigh(91,96)
 ).concat(
   arrayfromlowtohigh(123,126)
 )
-// values registered when checked off
-const rangeamount = numberselector.value
-const lowercaseletters = lowercaseletters.checked
-const uppercaseletters = uppercaseletters.checked
-const numbers = numbers.checked
-const specialcharacters = specialcharacters.checked
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+
 // Write password to the #password input
-function writePassword(rangeamount, lowercaseletters, uppercaseletters, specialcharacters, numbers) { 
-  console.log(lowercase_char_code)
-  String.fromCharCode(65)
-}
-function arrayfromlowtohigh(low, high){
-  for (let i = low; i <= high; i++){
-    array.push(i)
-  }
-  return array
-}
+function writePassword(rangeamount, lowercaseletters, uppercaseletters, specialcharacters, numbers) {
+  const rangeamount = numberselector.value
+  const lowercaseletters = lowercaseletters.checked
+  const uppercaseletters = uppercaseletters.checked
+  const numbers = numbers.checked
+  const specialcharacters = specialcharacters.checked
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+​
   passwordText.value = password; 
-
+​}
 
 // Add event listener to generate button
 var button = document.getElementById("generate");
@@ -60,4 +36,3 @@ document.getElementById("generate").addEventListener("click", function() {
     console.log("write password")
   }
 });
-button.addEventListener("click", writePassword);
