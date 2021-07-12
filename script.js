b9569e90034c35dcc16f5cd261adc541db0793c7
raria029 <rocio.arias98@gmail.com>
2021-07-12 18:34:34 -0400

    char codes

--- script.js
+++ script.js
@@ -8,22 +8,48 @@ function syncselector(e) {
   numberselector.value = value
   rangeselector.value = value
 }
+//Lowercase, Uppercase, Numbers, Special Characters
+const lowercaseletters = document.getElementById('lowercaseletters')
+const uppercaseletters = document.getElementById('uppercaseletters')
+const numbers = document.getElementById('numbers')
+const specialcharacters = document.getElementById('specialcharacters')
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+//Arrays to generate
+const lowercase_char_code = arrayfromlowtohigh(97,122)
+const uppercase_char_code = arrayfromlowtohigh(65,90)
+const numbers_char_code = arrayfromlowtohigh(48,57)
+const specialcharacters_char_code = arrayfromlowtohigh(33,47).concat(
+  arrayfromlowtohigh(58,64)
+).concat(
+  arrayfromlowtohigh(91,96)
+).concat(
+  arrayfromlowtohigh(123,126)
+)
+// values registered when checked off
+const rangeamount = numberselector.value
+const lowercaseletters = lowercaseletters.checked
+const uppercaseletters = uppercaseletters.checked
+const numbers = numbers.checked
+const specialcharacters = specialcharacters.checked
 // Write password to the #password input
-function writePassword(rangeamount, lowercaseletters, uppercaseletters, specialcharacters, numbers) {
-  const rangeamount = numberselector.value
-  const lowercaseletters = lowercaseletters.checked
-  const uppercaseletters = uppercaseletters.checked
-  const numbers = numbers.checked
-  const specialcharacters = specialcharacters.checked
+function writePassword(rangeamount, lowercaseletters, uppercaseletters, specialcharacters, numbers) { 
+  console.log(lowercase_char_code)
+  String.fromCharCode(65)
+}
+function arrayfromlowtohigh(low, high){
+  for (let i = low; i <= high; i++){
+    array.push(i)
+  }
+  return array
+}
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password; 
 
-}
+
 // Add event listener to generate button
 //document.getElementById("generate").onclick = function () {writePassword();
 document.getElementById("generate").addEventListener("click", function() {
